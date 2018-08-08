40f24e9510776b8b674f60b850160aebf3c41297
Giacomo Frisoni <giacomofrisoni@gmail.com>
2018-08-08 12:25:51 +0200

    Made some fixes

--- src/main/java/it/unibo/controller/TicketController.java
+++ src/main/java/it/unibo/controller/TicketController.java
@@ -22,29 +22,49 @@ public class TicketController {
     @Autowired RedisTemplate<String, Long> redis;
 
     @RequestMapping(value="/ticket/{roomId}", method = RequestMethod.GET)
-    public Long ticket(@PathVariable("roomId") final String roomId, final javax.servlet.http.HttpServletRequest req) {
+    public Long getTicket(@PathVariable("roomId") final String roomId, final javax.servlet.http.HttpServletRequest req) {
         
     	// Increments atomically the ticket value for the requested room
         final Long n = (Long) redis.execute(new SessionCallback<List<Object>>() {
         	@SuppressWarnings({ "rawtypes", "unchecked" })
             @Override
-            public List<Object> execute(RedisOperations ops) throws DataAccessException {
+            public List<Object> execute(final RedisOperations ops) throws DataAccessException {
                 ops.multi();
                 ops.opsForValue().increment(roomId, 1);
                 return ops.exec();
             }
         }).stream().reduce((a,b) -> b).get();
 
-    	
+        // Random delay in order to simulate concurrency
     	try {
 			Thread.sleep((long) (Math.random() * 1000));
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
     	
         return n;
         
+    }
+    
+    @RequestMapping(value="/ticket/{roomId}/count", method = RequestMethod.GET)
+    public Long countTicketsInRoom(@PathVariable("roomId") final String roomId, final javax.servlet.http.HttpServletRequest req) {
+        
+    	if (redis.hasKey(roomId)) {
+    		final Long n = (Long) redis.execute(new SessionCallback<List<Object>>() {
+            	@SuppressWarnings({ "rawtypes", "unchecked" })
+                @Override
+                public List<Object> execute(final RedisOperations ops) throws DataAccessException {
+                    ops.multi();
+                    ops.opsForValue().increment(roomId, 0);
+                    return ops.exec();
+                }
+            }).stream().reduce((a,b) -> b).get();
+    		return n;
+    	} else {
+    		// If there is no ticket value associated to the room, the counter is zero (new room)
+    		return 0L;
+    	}
+		
     }
     
     @RequestMapping(value="/ticket/{roomId}/reset", method = RequestMethod.POST)
@@ -54,7 +74,7 @@ public class TicketController {
     	redis.execute(new SessionCallback<List<Object>>() {
             @SuppressWarnings({ "rawtypes", "unchecked" })
 			@Override
-            public List<Object> execute(RedisOperations ops) throws DataAccessException {
+            public List<Object> execute(final RedisOperations ops) throws DataAccessException {
                 ops.multi();
                 ops.delete(roomId);
                 return ops.exec();
